5832f939478cbc32418c47d6ad57464491e36c18
danielammmm <danidanidjma@gmail.com>
2024-09-18 19:37:09 -0600

    clase symbol item

--- 04_TraduccionDirigidaPorSintaxis/src/SymbolItem.java
+++ 04_TraduccionDirigidaPorSintaxis/src/SymbolItem.java
@@ -14,42 +14,51 @@ public class SymbolItem {
     private int memoryAddress;
     private Object value;
 
-    public SymbolItem(String id, int function, int type, int scope, int memoryAddress, Object value){
-        setId(id);
-        setFunction(function);
-        setType(type);
-        setScope(scope);
-        setMemoryAddress(memoryAddress);
-        setValue(value);
+    public SymbolItem(String id, int function, int type, int scope, int memoryAddress, Object value) {
+        this.id = id;  // Asignar directamente
+        this.function = function;  // Asignar directamente
+        this.type = type;  // Asignar directamente
+        this.scope = scope;  // Asignar directamente
+        this.memoryAddress = memoryAddress;  // Asignar directamente
+        this.value = value;  // Asignar directamente
     }
 
     public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
+
     public int getFunction() {
         return function;
     }
+
     public void setFunction(int function) {
         this.function = function;
     }
+
     public int getType() {
         return type;
     }
+
     public void setType(int type) {
         this.type = type;
     }
+
     public int getScope() {
         return scope;
     }
+
     public void setScope(int scope) {
         this.scope = scope;
     }
+
     public int getMemoryAddress() {
         return memoryAddress;
     }
+
     public void setMemoryAddress(int memoryAddress) {
         this.memoryAddress = memoryAddress;
     }
@@ -57,8 +66,8 @@ public class SymbolItem {
     public Object getValue(){
         return value;
     }
+
     public void setValue(Object value){
         this.value = value;
     }
-    
 }
