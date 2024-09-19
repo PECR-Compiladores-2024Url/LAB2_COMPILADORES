38ced39a4355d8c130a79ef9cb7ed07754934c35
danielammmm <danidanidjma@gmail.com>
2024-09-18 19:36:00 -0600

    clase lexema

--- 04_TraduccionDirigidaPorSintaxis/src/Lexema.java
+++ 04_TraduccionDirigidaPorSintaxis/src/Lexema.java
@@ -1,19 +1,20 @@
-public class Lexema extends GrammarSymbol{
-    
+public class Lexema extends GrammarSymbol {
+
     private String value;
 
     public Lexema(String symbol, String value){
         super(TERMINAL);
-        setSymbol(symbol);
-        setValue(value);
-    }
-
-    public String getValue() {
-        return value;
-    }
-    public void setValue(String value) {
+        setSymbol(symbol);  // Usar setSymbol() en lugar de acceso directo
         this.value = value;
     }
 
-    
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
